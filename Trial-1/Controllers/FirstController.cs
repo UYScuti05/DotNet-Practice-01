b9a749dbca37a46ef922953b96bbf846eb7269f3
UY Scuti <anshul.sharma05@outlook.com>
2021-11-16 19:35:34 +0530

    initial commit

---
+++ Trial-1/Controllers/FirstController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trial_1.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
